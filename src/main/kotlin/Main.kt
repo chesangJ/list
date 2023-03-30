e1bff8e9d36edf2a7d354018b2b023da83651a33
wambui-nyambura <nyamburawambuui@gmail.com>
2023-03-30 18:06:08 +0300

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,29 @@
 fun main() {
+
 val x=names(listOf("Zero","One","Two","Three","Four"))
     println(x)
     val man= listOf(30.5,40.5,50.0,60.0,70.0,80.0)
     val (avg,total)=height(man)
     println(avg)
     println(total)
+  val vehicles= listOf(
+      Car("KAG996Y",1345.67),
+      Car("KDD768Z",1000.0),
+      Car("KBZ2348B",6589.09)
+
+  )
+    val mileage=average(vehicles)
+    println("My mileage $mileage")
+val z= mutableListOf(
+    Person("Great",24,4.6,75),
+            Person("Linda",10,3.6,45),
+    Person("Peter",44,5.6,85),
+    Person("Anna",14,1.6,25),
+
+
+)
+    println(z)
+
 
 }
 fun names(x:List<String>):List<String>{
@@ -19,18 +38,31 @@ fun height(m:List<Double>):Pair<Double,Double>{
     var total=m.sum()
     return Pair(avg,total)
 }
-data class person(var name:String,var age:Int,var height:Int,var weight:Int){
-    fun people(){
-        val prod1 =person("Joy",)
-        val switch = person()
-        val persons = listOf<person>(prod1, switch)
-        println(persons)
-        val sorted = persons.sortedBy { person -> person.name}
-        println(sorted)
+data class Person(var name:String,var age:Int,var height:Double,var weight:Int)
+fun age(person:List<Person>):List<Person>{
+    return person.sortedByDescending { it.age }
 
-
-
-
-
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+data class Car(val registration:String,val mileage:Double)
+ fun average(vehicles:List<Car>):Double{
+     var count=0.0
+     for (vehicle in vehicles){
+         count+=vehicle.mileage
+     }
+     return count /vehicles.size
+ }
+
+
+
